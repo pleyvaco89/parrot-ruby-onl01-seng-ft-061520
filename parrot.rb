7579d5e98a17f1882ce1c05ac45fe4936cd959d1
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-16 19:16:05 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase="Squawk" , parrot="Pretty bird")
-  puts "#{phrase}, #{parrot}
-end
 
-parrot
+ 
 
 
 
